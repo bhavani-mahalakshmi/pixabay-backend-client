b490b1062776eb8dfb91073c07218177283330f9
Bhavani Mahalakshmi Gowri Sankar <bgowrisa@asu.edu>
2023-05-31 14:47:32 -0700

    adding exception handling for apis

--- pixabay_backend_client/pixabay/views.py
+++ pixabay_backend_client/pixabay/views.py
@@ -7,34 +7,43 @@ from pixabay.result_formatter import format_image_detail, format_images_list
 PIXABAY_API_KEY = os.getenv("PIXABAY_API_KEY", "test")
 
 def image_list(request):
-    search_query = request.GET.get('q', '')
-    page = int(request.GET.get('page', 1))
-    
-    url = f"{PIXABAY_BASE_URL}?key={PIXABAY_API_KEY}&q={search_query}&page={page}&per_page={NO_OF_IMAGES_PER_PAGE}"
-    response = requests.get(url)
-    if response.status_code != 200:
+    try:
+        search_query = request.GET.get('q', '')
+        page = int(request.GET.get('page', 1))
+        
+        url = f"{PIXABAY_BASE_URL}?key={PIXABAY_API_KEY}&q={search_query}&page={page}&per_page={NO_OF_IMAGES_PER_PAGE}"
+        response = requests.get(url)
+        if response.status_code != 200:
+            return HttpResponseServerError("An error occurred while fetching images list.")
+        
+        data = response.json()
+        result = format_images_list(data)
+        return JsonResponse(result)
+    except Exception as e:
+        print(e)
         return HttpResponseServerError("An error occurred while fetching images list.")
     
-    data = response.json()
-    result = format_images_list(data)
-    return JsonResponse(result)
-
 def image_detail(request, image_id):
-    # Validate image ID
-    if not image_id:
-        return HttpResponseBadRequest("Image ID is required.")
-    
-    url = f"{PIXABAY_BASE_URL}?key={PIXABAY_API_KEY}&id={image_id}"
-    response = requests.get(url)
-    if response.status_code != 200:
+    try:
+        # Validate image ID
+        if not image_id:
+            return HttpResponseBadRequest("Image ID is required.")
+        
+        url = f"{PIXABAY_BASE_URL}?key={PIXABAY_API_KEY}&id={image_id}"
+        response = requests.get(url)
+        if response.status_code != 200:
+            return HttpResponseServerError("An error occurred while fetching image details.")
+        
+        data = response.json()
+        if 'hits' in data and len(data['hits']) > 0:
+            image = data['hits'][0]
+        else:
+            return HttpResponseBadRequest("Image with ID not found.")
+        
+        image_detail = format_image_detail(image)
+        
+        return JsonResponse(image_detail)
+    except Exception as e:
+        print(e)
         return HttpResponseServerError("An error occurred while fetching image details.")
     
-    data = response.json()
-    if 'hits' in data and len(data['hits']) > 0:
-        image = data['hits'][0]
-    else:
-        return HttpResponseBadRequest("Image with ID not found.")
-    
-    image_detail = format_image_detail(image)
-    
-    return JsonResponse(image_detail)
