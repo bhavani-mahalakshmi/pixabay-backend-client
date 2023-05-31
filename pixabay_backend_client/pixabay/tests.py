db332e2d7ba0e26e468384a0a21c4a1b751f9840
Bhavani Mahalakshmi Gowri Sankar <bgowrisa@asu.edu>
2023-05-31 14:31:44 -0700

    basic test cases

--- pixabay_backend_client/pixabay/tests.py
+++ pixabay_backend_client/pixabay/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class ImageSearchAPITest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        
+    def test_image_list_endpoint(self):
+        response = self.client.get('/image/list/')
+        self.assertEqual(response.status_code, 200)
+        
+    def test_image_list_with_search_query_and_page(self):
+        response = self.client.get('/image/list/?q=yellow flower&page=1')
+        self.assertEqual(response.status_code, 200)
+        
+    def test_image_detail_endpoint(self):
+        response = self.client.get('/image/2145539/')
+        self.assertEqual(response.status_code, 200)
