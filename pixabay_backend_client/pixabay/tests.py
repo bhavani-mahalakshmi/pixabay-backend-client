948a1c3f164544610c22d180b894a0105688c688
Bhavani Mahalakshmi Gowri Sankar <bgowrisa@asu.edu>
2023-05-31 15:05:37 -0700

    adding api key as env for test

--- pixabay_backend_client/pixabay/tests.py
+++ pixabay_backend_client/pixabay/tests.py
@@ -1,8 +1,10 @@
+import os
 from django.test import TestCase, Client
 
 class ImageSearchAPITest(TestCase):
     def setUp(self):
         self.client = Client()
+        os.environ["PIXABAY_API_KEY"] = "your_api_key_here"
 
     def test_image_list_endpoint(self):
         response = self.client.get('/image/list')
