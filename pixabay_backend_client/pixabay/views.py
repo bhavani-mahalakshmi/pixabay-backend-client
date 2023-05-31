dc5818b17d66c041389cd45bcc7dc441fdc96dfd
Bhavani Mahalakshmi Gowri Sankar <bgowrisa@asu.edu>
2023-05-31 14:07:37 -0700

    image list api

--- pixabay_backend_client/pixabay/views.py
+++ pixabay_backend_client/pixabay/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+import requests
+from django.http import JsonResponse
 
-# Create your views here.
+from pixabay.config import PIXABAY_API_KEY, NO_OF_IMAGES_PER_PAGE
+
+
+def image_list(request):
+    search_query = request.GET.get('q', '')
+    page = int(request.GET.get('page', 1))
+    
+    url = f"https://pixabay.com/api/?key={PIXABAY_API_KEY}&q={search_query}&page={page}&per_page={NO_OF_IMAGES_PER_PAGE}"
+    response = requests.get(url)
+    data = response.json()
+    if 'hits' in data:
+        images = data['hits']
+    else:
+        images = []
+    
+    return JsonResponse(images)
--- pixabay_backend_client/pixabay_backend_client/urls.py
+++ pixabay_backend_client/pixabay_backend_client/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pixabay import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('image/list/', views.image_list, name='image_list')
 ]
